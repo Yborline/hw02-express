cc2cba83e8f2f0bfe6463285e458b5354a29ec26
Yaroslav Borys <borrline@gmail.com>
2022-04-20 14:27:43 +0300

    finish2222

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// const { response } = require("express");
-// const { request } = require("express");
 const express = require("express");
-// const fs = require("fs").promises;
-// const moment = require("moment");
-//
 const morgan = require("morgan");
-//
 const cors = require("cors");
-const contacts = require("./models/contacts.json");
-
-//
-//
-
+// const contacts = require("./models/contacts.json");
 const logger = require("morgan");
-// const cors = require("cors");
 
 const { contactsRouter } = require("./routes/api/contacts");
 
@@ -26,18 +15,6 @@ const app = express();
 //   next();
 // });
 
-app.get("/", (request, response) => {
-  // console.log(request.url);
-  // console.log(request.method);
-  // console.log(request.headers);
-  response.send("<h2>Home page</h2>");
-});
-
-app.get("/contacts", (req, res) => {
-  res.json(contacts);
-  // res.send(contacts);
-});
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
@@ -46,13 +23,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Not found" });
-// });
-
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message });
-// });
 
 module.exports = app;
